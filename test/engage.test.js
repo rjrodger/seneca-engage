9c1278073de1976f435a9bcacd2f381f01eeccf7
Richard Rodger <richard@ricebridge.com>
2013-02-19 00:59:43 +0000

    works over web

--- test/engage.test.js
+++ test/engage.test.js
@@ -26,12 +26,49 @@ describe('engage', function() {
 
   it('happy', function() {
     engagement.set({key:'k1',value:'v1'},function(err,token){
-      console.log(err,token)
       assert.ok(null==err)
       assert.ok(null!=token)
       assert.ok(_.isString(token))
       assert.ok(0 < token.length)
-    })
 
+      
+      engagement.get({key:'k1',token:token},function(err,value){
+        assert.ok(null==err)
+        assert.ok(null!=value)
+        assert.ok(_.isString(value))
+        assert.equal(value,'v1')
+      })
+    })
   })
+
+  it('context', function() {
+    var ctxt = {}
+    engagement.set({key:'k2',value:'v2',context:ctxt},function(err,token){
+      assert.ok(null==err)
+      assert.ok(0 < token.length)
+      assert.equal( ctxt.engage_token, token )
+
+      engagement.get({key:'k2',token:token,context:ctxt},function(err,value){
+        assert.ok(null==err)
+        assert.ok(null!=value)
+        assert.ok(_.isString(value))
+        assert.equal(value,'v2')
+      })
+    })
+  })
+
+  it('reqres', function() {
+    var req = {}, res = {cookie:function(k,v){req.seneca={engage_token:v}}}
+    engagement.set({key:'k3',value:'v3',req:req,res:res},function(err,token){
+      assert.ok(null==err)
+      assert.ok(0 < token.length)
+      assert.equal( req.seneca.engage_token, token )
+
+      engagement.get({key:'k3',token:token,req:req,res:res},function(err,value){
+        assert.ok(null==err)
+        assert.equal(value,'v3')
+      })
+    })
+  })
+  
 })
